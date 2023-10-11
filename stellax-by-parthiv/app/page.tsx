ad4f5a47ccdeaf37db6bf59bb61940c21c6d3b72
SKParthiv <142598464+SKParthiv@users.noreply.github.com>
2023-10-11 22:17:03 +0530

    Update

--- stellax-by-parthiv/app/page.tsx
+++ stellax-by-parthiv/app/page.tsx
@@ -1,113 +1,128 @@
+import Head from 'next/head'
 import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div>
+        <Head>
+            <title>StellaX</title>
+            <link rel='icon' href='/media/logo.png'/>
+        </Head>
+        <div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            src="/media/logo.png"
+            height={500}
+            width={500}
+            alt='LogoOfStellaX'
+            priority
             />
-          </a>
         </div>
-      </div>
+        <main>
+            <section>
+            <div>
+                <h1>Let's build a buisness together</h1>
+                <p>
+                    With StellaX, you can finally say goodbye to the long and tiring web and applicaiton development required for your business. Schedule a meet with us and get professional expertise from the software developers in StellaX and grow your business online as you've always wanted to be.
+                </p>
+                </div>
+                <div>
+                    <Image
+                    src='/media/hero.svg'
+                    height={500}
+            width={500}
+                    alt='Cool and animated hero'
+                    />
+                </div>
+                <button type='button'>Schedule A Meet</button>
+            </section>
+            <section>
+                <div>
+                    <p>Trusted by many buisnesses across the globe</p>
+                    <Image
+                    src='/'
+                    height={500}
+                    width={500}
+                    alt='Business1'
+                    />
+                    <Image
+                    src='/'
+                    height={500}
+                    width={500}
+                    alt='Buisness2'
+                    />
+                    <Image
+                    src='/'
+                    height={500}
+                    width={500}
+                    alt='Buisness3'
+                    />
+                </div>
+            </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <section>
+                <div>
+                    <Image
+                    src='/media/Section2.svg'
+                    height={500}
+                    width={500}
+                    alt='Need More Arguments Image'
+                    />
+                </div>
+                <div>
+                    <h2>Need More Arguments?</h2>
+                    <ul>
+                        <li>
+                            <svg></svg>
+                            <p>Hosting and publishing will be included and flexible.</p>
+                        </li>
+                        <li>
+                            <svg></svg>
+                            <p>Fulfills all data protection standards</p>
+                        </li>
+                        <li>
+                            <svg></svg>
+                            <p>Built by the world's most innovative software experts</p>
+                        </li>
+                    </ul>
+                    <button type='button'>Schedule A Meet</button>
+                </div>
+            </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <section>
+                <div>
+                    <h1>
+                        "We just love how Developers from StellaX turn the business to online and grow into tangible insights. Keep on rocking!"
+                    </h1>
+                    <p>Rama Murthy, Vice-President of GITAM Aero Astro</p>
+                    <div>
+                        <Image
+                        src='/media/PlaceholderofComments.svg'
+                        height={500}
+                        width={500}
+                        alt="Person's Photo"
+                        />
+                    </div>
+                </div>
+            </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <section>
+                <div>
+                    <Image
+                    src='/media/review4u.svg'
+                    height={500}
+                    width={500}
+                    alt='5 stars in Review4u'
+                    />
+                    <Image
+                    src='/media/RatingFox.svg'
+                    height={500}
+                    width={500}
+                    alt='5 stars in Rating Fox'
+                    />
+                </div>
+            </section>
+        </main>
+        </div>
   )
 }
